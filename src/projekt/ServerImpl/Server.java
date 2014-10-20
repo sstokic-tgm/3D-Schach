0607819a811b652531bf19e0871587b61953edce
sstokic-tgm <sstokic@student.tgm.ac.at>
2014-10-20 21:35:24 +0200

    initial commit

---
+++ src/projekt/ServerImpl/Server.java
@@ -0,0 +1,15 @@
+package projekt.ServerImpl;
+
+/**
+ * Server implementation
+ * 
+ * @author Stokic Stefan
+ * @version 0.1
+ */
+public class Server {
+
+	public static void main(String[] args) {
+
+
+	}
+}
