1a46c4661db07c2737b145ceda4dfdfc5e55b794
sstokic-tgm <sstokic@student.tgm.ac.at>
2014-11-10 16:45:57 +0100

    Client connection test added

---
+++ src/projekt/Client/Test/TestClient.java
@@ -0,0 +1,64 @@
+package projekt.Client.Test;
+
+
+import java.io.IOException;
+
+import projekt.ServerImpl.Packets.*;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.rmi.ObjectSpace;
+
+
+public class TestClient {
+
+	private Client client;
+	private AuthenticationPacket loginPacket;
+	private boolean login = false;
+
+	public TestClient() {
+
+		client = new Client();
+		client.start();
+
+		registerPackets();
+
+		loginPacket = ObjectSpace.getRemoteObject(client, 1, AuthenticationPacket.class);
+
+		new Thread("Connect") {
+			public void run () {
+				try {
+					client.connect(5000, "localhost", 54555);
+					
+					login = loginPacket.login("test", "test");
+					
+					System.out.print("Login... Connection status: ");
+					
+					if(login == true)
+						System.out.print("true");
+					else
+						System.out.print("false");
+					
+					System.out.println("\nDisconnecting... Connection status: " + loginPacket.disconnect());
+					
+				} catch (IOException ex) {
+					System.out.println("" + ex.getMessage());
+					System.exit(1);
+				}
+			}
+		}.start();
+	}
+
+	private void registerPackets() {
+
+		Kryo kryo = client.getKryo();
+
+		ObjectSpace.registerClasses(kryo);
+		kryo.register(AuthenticationPacket.class);
+	}
+	
+	public static void main(String[] args) {
+		
+		new TestClient();
+	}
+}
