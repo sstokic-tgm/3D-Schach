15e2123c6cf2bf22e05d6a2f208978b203a76e7a
sstokic-tgm <sstokic@student.tgm.ac.at>
2014-11-18 17:10:22 +0100

    register test implemented

--- src/projekt/Client/Test/TestClient.java
+++ src/projekt/Client/Test/TestClient.java
@@ -13,60 +13,113 @@ import com.esotericsoftware.kryonet.rmi.ObjectSpace;
 public class TestClient {
 
 	private Client client;
-	private AuthenticationPacket loginPacket;
+	private AuthenticationPacket authPacket;
 	private boolean login = false;
-	private String username;
-	
+	private boolean register = false;
 
-	public TestClient(String username, String password) {
 
-		this.username = username;
-		
+	public void loginTestClient(String username, String password) {
+
 		client = new Client();
 		client.start();
 
 		registerPackets();
 
-		loginPacket = ObjectSpace.getRemoteObject(client, 1, AuthenticationPacket.class);
+		authPacket = ObjectSpace.getRemoteObject(client, 1, AuthenticationPacket.class);
 
-//		new Thread("Connect") {
-//			public void run () {
-//				try {
-//					client.connect(5000, "localhost", 54555);
-//					
-//					login = loginPacket.login("test", "test");
-//					
-//					System.out.print("Login... Connection status: ");
-//					
-//					if(login == true)
-//						System.out.print("true");
-//					else
-//						System.out.print("false");
-//					
-//					System.out.println("\nDisconnecting... Connection status: " + loginPacket.disconnect());
-//					
-//				} catch (IOException ex) {
-//					System.out.println("" + ex.getMessage());
-//					System.exit(1);
-//				}
-//			}
-//		}.start();
-		
+		//		new Thread("Connect") {
+		//			public void run () {
+		//				try {
+		//					client.connect(5000, "localhost", 54555);
+		//					
+		//					login = loginPacket.login("test", "test");
+		//					
+		//					System.out.print("Login... Connection status: ");
+		//					
+		//					if(login == true)
+		//						System.out.print("true");
+		//					else
+		//						System.out.print("false");
+		//					
+		//					System.out.println("\nDisconnecting... Connection status: " + loginPacket.disconnect());
+		//					
+		//				} catch (IOException ex) {
+		//					System.out.println("" + ex.getMessage());
+		//					System.exit(1);
+		//				}
+		//			}
+		//		}.start();
+
 		new Thread( () -> {
 			try {
 				client.connect(5000, "localhost", 54555);
-				
-				login = loginPacket.login(username, password);
-				
+
+				login = authPacket.login(username, password);
+
 				System.out.print("Login... Connection status: ");
-				
+
 				if(login == true)
 					System.out.print("true");
 				else
 					System.out.print("false");
-				
-				System.out.println("\nDisconnecting... Connection status: " + loginPacket.disconnect());
-				
+
+				System.out.println("\nDisconnecting... Connection status: " + authPacket.disconnect());
+
+			} catch (IOException ex) {
+				System.out.println("" + ex.getMessage());
+				System.exit(1);
+			}
+		}).start();
+	}
+
+	public void registerTestClient(String username, String password) {
+
+
+		client = new Client();
+		client.start();
+
+		registerPackets();
+
+		authPacket = ObjectSpace.getRemoteObject(client, 1, AuthenticationPacket.class);
+
+		//		new Thread("Connect") {
+		//			public void run () {
+		//				try {
+		//					client.connect(5000, "localhost", 54555);
+		//					
+		//					login = loginPacket.login("test", "test");
+		//					
+		//					System.out.print("Login... Connection status: ");
+		//					
+		//					if(login == true)
+		//						System.out.print("true");
+		//					else
+		//						System.out.print("false");
+		//					
+		//					System.out.println("\nDisconnecting... Connection status: " + loginPacket.disconnect());
+		//					
+		//				} catch (IOException ex) {
+		//					System.out.println("" + ex.getMessage());
+		//					System.exit(1);
+		//				}
+		//			}
+		//		}.start();
+
+		new Thread( () -> {
+			try {
+				client.connect(5000, "localhost", 54555);
+
+				register = authPacket.register(username, password);
+
+				System.out.print("Register... Registered: ");
+
+				if(register == true)
+					System.out.print("true");
+				else
+					System.out.print("false");
+
+				System.out.println("\nDisconnecting... Connection status: " + authPacket.disconnect());
+
 			} catch (IOException ex) {
 				System.out.println("" + ex.getMessage());
 				System.exit(1);
